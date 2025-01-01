c467caa5155254a79046249c9ae1f7b1301a6bbe
Andre Costa <andrecondecosta@gmail.com>
2025-01-01 23:01:40 +0000

    modified:   src/components/ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import jwtDecode from 'jwt-decode';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ element }) => {
-  const token = localStorage.getItem('authToken');  // Verifique a chave 'authToken'
+  const token = localStorage.getItem('authToken');
 
-  return token ? element : <Navigate to="/login"/>;
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  try {
+    const decoded = jwtDecode(token);
+    const isExpired = decoded.exp * 1000 < Date.now();
+
+    if (isExpired) {
+      localStorage.removeItem('authToken');  // Remove token expirado
+      return <Navigate to="/login" />;
+    }
+
+    return element;  // Token válido
+  } catch (err) {
+    console.error('Invalid token', err);
+    localStorage.removeItem('authToken');
+    return <Navigate to="/login" />;
+  }
 };
 
 export default ProtectedRoute;
