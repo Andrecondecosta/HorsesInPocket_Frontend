77d9dcc67d335e88b5009c7199304b7ad0b33310
Andre Costa <andrecondecosta@gmail.com>
2025-01-06 16:12:44 +0000

    modified:   src/pages/DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
 import CardSection from '../components/CardSection';
 import HistoryTable from '../components/HistoryTable';
 import './DashboardPage.css';
 
 const DashboardPage = ({ setIsLoggedIn }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <Layout setIsLoggedIn={setIsLoggedIn}>
       <div className="dashboard-container">
-      <h2 className="dashboard-title">Dashboard</h2>
+        <h2 className="dashboard-title">Dashboard</h2>
         <CardSection />
         <HistoryTable />
       </div>
